b9e26af77637164ede184f1b8a63e1dd30f041a8
JosepSampe <jsampe@gmail.com>
2016-10-25 16:20:16 +0200

    Name changed

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
-paste_factory = ['crystal_introspection_handler = '
-                 'crystal_introspection_middleware.crystal_introspection_handler:filter_factory']
+paste_factory = ['crystal_metric_middleware = '
+                 'crystal_metric_middleware.crystal_metric_handler:filter_factory']
 
-setup(name='swift_crystal_introspection_middleware',
-      version='0.0.1',
-      description='Crystal Introspection middleware for OpenStack Swift',
+setup(name='swift_crystal_metric_middleware',
+      version='0.0.3',
+      description='Crystal metric middleware for OpenStack Swift',
       author='The AST-IOStack Team: Josep Sampe, Raul Gracia',
       url='http://iostack.eu',
       packages=find_packages(),
       requires=['swift(>=1.4)'],
-      entry_points={'paste.filter_factory':paste_factory}
+      entry_points={'paste.filter_factory': paste_factory}
       )

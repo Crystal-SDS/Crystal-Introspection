028100694b6ee26379bf6aa94789729b8b9f07eb
JosepSampe <jsampe@gmail.com>
2017-01-10 15:32:15 +0100

    Bug fix

--- crystal_metric_middleware/crystal_metric_control.py
+++ crystal_metric_middleware/crystal_metric_control.py
@@ -9,13 +9,13 @@ import json
 import os
 
 
-
 SRC_METRIC_PATH = os.path.join("/opt", "crystal", "workload_metrics")
-DST_METRIC_PATH = os.path.abspath(__file__).rsplit('/',1)[0]+'/metrics'
+DST_METRIC_PATH = os.path.abspath(__file__).rsplit('/', 1)[0]+'/metrics'
 
 
 class Singleton(type):
     _instances = {}
+
     def __call__(cls, *args, **kwargs):  # @NoSelf
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
@@ -24,100 +24,107 @@ class Singleton(type):
 
 class CrystalMetricControl(object):
     __metaclass__ = Singleton
-    
+
     def __init__(self, conf, log):
         self.logger = log
         self.conf = conf
-        
+
         self.status_thread = NodeStatusThread(self.conf, self.logger)
         self.status_thread.daemon = True
         self.status_thread.start()
-        
+
         self.control_thread = ControlThread(self.conf, self.logger)
         self.control_thread.daemon = True
-        
+
         self.publish_thread = PublishThread(self.conf, self.logger)
         self.publish_thread.daemon = True
-        
+
         self.threads_started = False
 
     def get_metrics(self):
-        return self.control_thread.metric_list 
-    
-    def publish_stateful_metric(self,routing_key, key, value):
+        return self.control_thread.metric_list
+
+    def publish_stateful_metric(self, routing_key, key, value):
         self.publish_thread.publish_statefull(routing_key, key, value)
-    
-    def publish_stateless_metric(self,routing_key, key, value):
+
+    def publish_stateless_metric(self, routing_key, key, value):
         self.publish_thread.publish_stateless(routing_key, key, value)
-        
-    def force_publish_metric(self,routing_key, key, value):
+
+    def force_publish_metric(self, routing_key, key, value):
         self.publish_thread.force_publish_metric(routing_key, key, value)
 
 
 class PublishThread(Thread):
-    
+
     def __init__(self, conf, logger):
         Thread.__init__(self)
-        
+
         self.logger = logger
         self.monitoring_statefull_data = dict()
         self.monitoring_stateless_data = dict()
-        
-        self.interval = conf.get('publish_interval',0.995)
-        #self.ip = conf.get('bind_ip')+":"+conf.get('bind_port')
+
+        self.interval = conf.get('publish_interval', 0.995)
+        # self.ip = conf.get('bind_ip')+":"+conf.get('bind_port')
         self.host_name = socket.gethostname()
         self.exchange = conf.get('exchange', 'amq.topic')
-        
+
         rabbit_host = conf.get('rabbit_host')
         rabbit_port = int(conf.get('rabbit_port'))
         rabbit_user = conf.get('rabbit_username')
         rabbit_pass = conf.get('rabbit_password')
 
-        credentials = pika.PlainCredentials(rabbit_user,rabbit_pass)  
-        self.parameters = pika.ConnectionParameters(host = rabbit_host,
-                                                    port = rabbit_port,
-                                                    credentials = credentials)
-      
+        credentials = pika.PlainCredentials(rabbit_user, rabbit_pass)
+        self.parameters = pika.ConnectionParameters(host=rabbit_host,
+                                                    port=rabbit_port,
+                                                    credentials=credentials)
+
     def publish_statefull(self, routing_key, key, value):
-        if not routing_key in self.monitoring_statefull_data:
-            self.monitoring_statefull_data[routing_key] = dict()         
-            
-        if not key in self.monitoring_statefull_data[routing_key]:
+        if routing_key not in self.monitoring_statefull_data:
+            self.monitoring_statefull_data[routing_key] = dict()
+
+        if key not in self.monitoring_statefull_data[routing_key]:
             self.monitoring_statefull_data[routing_key][key] = 0
 
-        self.monitoring_statefull_data[routing_key][key] += value
-            
+        try:
+            self.monitoring_statefull_data[routing_key][key] += value
+        except:
+            pass
+
     def publish_stateless(self, routing_key, key, value):
-        if not routing_key in self.monitoring_stateless_data:
+        if routing_key not in self.monitoring_stateless_data:
             self.monitoring_stateless_data[routing_key] = dict()
-                
-        if not key in self.monitoring_stateless_data[routing_key]:
+
+        if key not in self.monitoring_stateless_data[routing_key]:
             self.monitoring_stateless_data[routing_key][key] = 0
-                  
-        self.monitoring_stateless_data[routing_key][key] += value
-        
+
+        try:
+            self.monitoring_stateless_data[routing_key][key] += value
+        except:
+            pass
+
     def force_publish_metric(self, routing_key, key, value):
         date = datetime.now(pytz.timezone(time.tzname[0]))
         rabbit = pika.BlockingConnection(self.parameters)
         channel = rabbit.channel()
-        
+
         data = dict()
         data[self.host_name] = dict()
         data[self.host_name][key] = value
         data[self.host_name]['@timestamp'] = str(date.isoformat())
 
-        channel.basic_publish(exchange=self.exchange, 
-                              routing_key=routing_key, 
+        channel.basic_publish(exchange=self.exchange,
+                              routing_key=routing_key,
                               body=json.dumps(data))
+        rabbit.close()
 
     def run(self):
         data = dict()
+        rabbit = pika.BlockingConnection(self.parameters)
+        channel = rabbit.channel()
         while True:
             time.sleep(self.interval)
             date = datetime.now(pytz.timezone(time.tzname[0]))
-            rabbit = pika.BlockingConnection(self.parameters)
-            channel = rabbit.channel()
-            
+
             for routing_key in self.monitoring_stateless_data.keys():
                 data[self.host_name] = self.monitoring_stateless_data[routing_key].copy()
                 data[self.host_name]['@timestamp'] = str(date.isoformat())
@@ -126,55 +133,54 @@ class PublishThread(Thread):
                         del self.monitoring_stateless_data[routing_key][key]
                     else:
                         self.monitoring_stateless_data[routing_key][key] = 0
-                
+
                 if not self.monitoring_stateless_data[routing_key]:
                     del self.monitoring_stateless_data[routing_key]
 
-                channel.basic_publish(exchange=self.exchange, 
-                                      routing_key=routing_key, 
+                channel.basic_publish(exchange=self.exchange,
+                                      routing_key=routing_key,
                                       body=json.dumps(data))
-            
+
             for routing_key in self.monitoring_statefull_data.keys():
                 data[self.host_name] = self.monitoring_statefull_data[routing_key].copy()
                 data[self.host_name]['@timestamp'] = str(date.isoformat())
                 for key in self.monitoring_statefull_data[routing_key].keys():
                     if self.monitoring_statefull_data[routing_key][key] == 0:
                         del self.monitoring_statefull_data[routing_key][key]
-                        
+
                 if not self.monitoring_statefull_data[routing_key]:
                     del self.monitoring_statefull_data[routing_key]
-                
-                channel.basic_publish(exchange=self.exchange, 
-                                      routing_key=routing_key, 
+
+                channel.basic_publish(exchange=self.exchange,
+                                      routing_key=routing_key,
                                       body=json.dumps(data))
-                
 
 
 class ControlThread(Thread):
-    
+
     def __init__(self, conf, logger):
         Thread.__init__(self)
-        
+
         self.conf = conf
         self.logger = logger
         self.server = self.conf.get('execution_server')
-        self.interval = self.conf.get('control_interval',10)
+        self.interval = self.conf.get('control_interval', 10)
         redis_host = self.conf.get('redis_host')
         redis_port = self.conf.get('redis_port')
         redis_db = self.conf.get('redis_db')
-        
+
         self.redis = redis.StrictRedis(redis_host,
                                        redis_port,
                                        redis_db)
-        
+
         self.metric_list = {}
-    
+
     def _get_workload_metrics(self):
         """
         This method connects to redis to download new metrics the information
         introduced via the dashboard.
         """
-        metric_keys = self.redis.keys("workload_metric:*") 
+        metric_keys = self.redis.keys("workload_metric:*")
         metric_list = dict()
         for key in metric_keys:
             metric = self.redis.hgetall(key)
@@ -198,28 +204,28 @@ class ControlThread(Thread):
 
 
 class NodeStatusThread(Thread):
-    
+
     def __init__(self, conf, logger):
         Thread.__init__(self)
-        
+
         self.conf = conf
         self.logger = logger
         self.server = self.conf.get('execution_server')
-        self.interval = self.conf.get('status_interval',10)
+        self.interval = self.conf.get('status_interval', 10)
         redis_host = self.conf.get('redis_host')
         redis_port = self.conf.get('redis_port')
         redis_db = self.conf.get('redis_db')
-        
+
         self.host_name = socket.gethostname()
-        self.host_ip = socket.gethostbyname(self.host_name)   
+        self.host_ip = socket.gethostbyname(self.host_name)
         self.devices = self.conf.get('devices')
-        
+
         self.redis = redis.StrictRedis(redis_host,
                                        redis_port,
                                        redis_db)
-        
+
         self.metric_list = {}
-        
+
     def _get_swift_disk_usage(self):
         swift_devices = dict()
         if self.devices and os.path.exists(self.devices):
@@ -229,15 +235,15 @@ class NodeStatusThread(Thread):
                     swift_devices[disk] = dict()
                     swift_devices[disk]['size'] = statvfs.f_frsize * statvfs.f_blocks
                     swift_devices[disk]['free'] = statvfs.f_frsize * statvfs.f_bfree
-      
+
         return swift_devices
-    
+
     def run(self):
         while True:
             swift_usage = self._get_swift_disk_usage()
-            self.redis.hmset('node:'+self.host_name, {'type':self.server,
-                                                      'name':self.host_name,
-                                                      'ip':self.host_ip,
-                                                      'last_ping':time.time(),
-                                                      'devices':json.dumps(swift_usage)})
+            self.redis.hmset('node:'+self.host_name, {'type': self.server,
+                                                      'name': self.host_name,
+                                                      'ip': self.host_ip,
+                                                      'last_ping': time.time(),
+                                                      'devices': json.dumps(swift_usage)})
             time.sleep(self.interval)
--- crystal_metric_middleware/crystal_metric_handler.py
+++ crystal_metric_middleware/crystal_metric_handler.py
@@ -8,8 +8,10 @@ import time
 
 PACKAGE_NAME = __name__.split('.')[0]
 
+
 class NotCrystalRequest(Exception):
     pass
+
 
 def _request_instance_property():
     """
@@ -43,11 +45,11 @@ class CrystalMetricHandler(object):
         self.logger = logger
         self.conf = conf
         self.request = request
-        self.method = self.request.method 
+        self.method = self.request.method
         self.response = None
         self.crystal_control = crystal_control
         self._start_control_threads()
-        
+
     def _extract_vaco(self):
         """
         Set version, account, container, obj vars from self._parse_vaco result
@@ -63,7 +65,7 @@ class CrystalMetricHandler(object):
                 self.request.split_path(5, 5, rest_with_last=True)
         else:
             raise NotCrystalRequest()
-        
+
     def _start_control_threads(self):
         if not self.crystal_control.threads_started:
             try:
@@ -74,56 +76,56 @@ class CrystalMetricHandler(object):
                 time.sleep(0.1)
             except:
                 self.logger.info("Crystal Metric - Error starting threads.")
-            
-    def _import_metric(self,metric):
+
+    def _import_metric(self, metric):
         modulename = 'metrics.'+metric['metric_name'].rsplit('.', 1)[0]
-        classname = metric['class_name']        
+        classname = metric['class_name']
         m = __import__(PACKAGE_NAME+'.'+modulename, globals(), locals(), [classname])
-        m_class = getattr(m, classname) 
-        metric_class = m_class(self.logger, self.crystal_control, modulename, 
+        m_class = getattr(m, classname)
+        metric_class = m_class(self.logger, self.crystal_control, modulename,
                                self.exec_server, self.request, self.response)
         return metric_class
-    
+
     @property
     def _is_valid_request(self):
         return self.method == 'GET' or self.method == 'PUT'
 
     def handle_request(self):
-        
+
         if self._is_valid_request:
             metrics = self.crystal_control.get_metrics()
-            
+
             for metric_key in metrics:
                 metric = metrics[metric_key]
                 if metric['in_flow'] == 'True':
-                    metric_class = self._import_metric(metric)            
+                    metric_class = self._import_metric(metric)
                     self.request = metric_class.execute()
-    
+
             if hasattr(self.request.environ['wsgi.input'], 'metrics'):
                 metric_list = list()
                 for metric in self.request.environ['wsgi.input'].metrics:
                     metric_list.append(metric.metric_name.split('.')[1])
                 self.logger.info('Crystal Metric - Go to execute '
                                  'metrics on input flow: ' + str(metric_list))
-            
+
             self.response = self.request.get_response(self.app)
-            
-            if self.response.is_success:            
+
+            if self.response.is_success:
                 for metric_key in metrics:
                     metric = metrics[metric_key]
                     if metric['out_flow'] == 'True':
-                        metric_class = self._import_metric(metric)            
+                        metric_class = self._import_metric(metric)
                         self.response = metric_class.execute()
-            
+
             if hasattr(self.response.app_iter, 'metrics'):
                 metric_list = list()
                 for metric in self.response.app_iter.metrics:
                     metric_list.append(metric.metric_name.split('.')[1])
                 self.logger.info('Crystal Introspection - Go to execute '
                                  'metrics on output flow: ' + str(metric_list))
-            
+
             return self.response
-        
+
         return self.request.get_response(self.app)
 
 
@@ -136,11 +138,11 @@ class CrystalMetricHandlerMiddleware(object):
         self.conf = crystal_conf
         self.handler_class = CrystalMetricHandler
         self.control_class = CrystalMetricControl
-        
+
         ''' Singleton instance of Metric control '''
-        self.crystal_control =  self.control_class(conf = self.conf,
-                                                   log = self.logger)
-        
+        self.crystal_control = self.control_class(conf=self.conf,
+                                                  log=self.logger)
+
     @wsgify
     def __call__(self, req):
         try:
@@ -151,7 +153,7 @@ class CrystalMetricHandlerMiddleware(object):
                                                  self.crystal_control)
             self.logger.debug('crystal_metric_handler call')
         except NotCrystalRequest:
-            return req.get_response(self.app)            
+            return req.get_response(self.app)
 
         try:
             return request_handler.handle_request()
@@ -165,24 +167,24 @@ class CrystalMetricHandlerMiddleware(object):
 
 def filter_factory(global_conf, **local_conf):
     """Standard filter factory to use the middleware with paste.deploy"""
-    
+
     conf = global_conf.copy()
     conf.update(local_conf)
 
     crystal_conf = dict()
     crystal_conf['execution_server'] = conf.get('execution_server', 'object')
-    
+
     crystal_conf['rabbit_host'] = conf.get('rabbit_host', 'controller')
     crystal_conf['rabbit_port'] = int(conf.get('rabbit_port', 5672))
     crystal_conf['rabbit_username'] = conf.get('rabbit_username', 'openstack')
     crystal_conf['rabbit_password'] = conf.get('rabbit_password', 'rabbitmqastl1a4b4')
-    #crystal_conf['rabbit_username'] = conf.get('rabbit_username', 'test')
-    #crystal_conf['rabbit_password'] = conf.get('rabbit_password', 'test')
+    # crystal_conf['rabbit_username'] = conf.get('rabbit_username', 'test')
+    # crystal_conf['rabbit_password'] = conf.get('rabbit_password', 'test')
 
     crystal_conf['redis_host'] = conf.get('redis_host', 'controller')
     crystal_conf['redis_port'] = int(conf.get('redis_port', 6379))
     crystal_conf['redis_db'] = int(conf.get('redis_db', 0))
-    
+
     crystal_conf['bind_ip'] = conf.get('bind_ip')
     crystal_conf['bind_port'] = conf.get('bind_port')
     crystal_conf['devices'] = conf.get('devices')

fb8300edda76ea64885ca965a8f68b8d1fa44077
JosepSampe <jsampe@gmail.com>
2017-09-15 11:15:14 +0200

    Fix: duplicated line

--- crystal_metric_middleware/metrics/metric_control.py
+++ crystal_metric_middleware/metrics/metric_control.py
@@ -21,8 +21,7 @@ class Singleton(type):
 
     def __call__(cls, *args, **kwargs):  # @NoSelf
         if cls not in cls._instances:
-            if cls not in cls._instances:
-                cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
 
 

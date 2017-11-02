9aa567a1edb0a1f6d51b6813f4262ac2a0cf7d64
JosepSampe <jsampe@gmail.com>
2017-11-02 16:18:53 +0100

    Changed bandwidth metric

--- metric_samples/bandwidth.py
+++ metric_samples/bandwidth.py
@@ -1,22 +1,10 @@
 from crystal_metric_middleware.metrics.abstract_metric import AbstractMetric
-import eventlet
 
 
 class Bandwidth(AbstractMetric):
 
     type = 'stateless'
 
-    def on_start(self):
-        self.mbytes = 0
-        self.sender = eventlet.spawn(self.send_metric)
-
     def on_read(self, chunk):
-        self.mbytes += (len(chunk)/1024.0)/1024
-
-    def send_metric(self):
-        while True:
-            eventlet.sleep(0.1)
-            if self.mbytes == 0:
-                break
-            self.register_metric(self.mbytes)
-            self.mbytes = 0
+        mbytes = (len(chunk)/1024.0)/1024
+        self.register_metric(mbytes)
